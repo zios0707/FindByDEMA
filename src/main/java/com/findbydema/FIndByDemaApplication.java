2d2355c5268ec23340635ceec8d6c7693722eac1
zios <khyunwook0707@gmail.com>
2023-10-04 10:35:57 +0900

    first commit

---
+++ src/main/java/com/findbydema/FIndByDemaApplication.java
@@ -0,0 +1,13 @@
+package com.findbydema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FIndByDemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FIndByDemaApplication.class, args);
+	}
+
+}
