878346ce88614bd9d45330abacc58215d0fd5be3
zios <khyunwook0707@gmail.com>
2023-10-06 11:37:47 +0900

    feat // 유저 찾기 예외
    유저 찾기 예외처리 만듦

---
+++ src/main/java/com/findbydema/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,14 @@
+package com.findbydema.domain.user.exception;
+
+import com.findbydema.global.exception.CustomException;
+import com.findbydema.global.exception.error.ErrorCode;
+
+public class UserNotFoundException extends CustomException {
+    public  static final CustomException EXCEPTION = new UserNotFoundException();
+
+    private UserNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+
+
+}
