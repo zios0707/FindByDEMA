cfb8b856a4b5798cd52d469a31513c507e0ccd2c
zios <khyunwook0707@gmail.com>
2023-11-21 20:07:30 +0900

    refactor //

--- src/main/java/com/findbydema/domain/livechat/controller/ChatController.java
+++ src/main/java/com/findbydema/domain/livechat/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.findbydema.domain.livechat.controller.dto.request.MessageRequest;
 import com.findbydema.domain.livechat.entity.ChatRecord;
 import com.findbydema.domain.livechat.service.GetMessageListService;
 import com.findbydema.domain.livechat.service.SaveMessageService;
+import com.findbydema.domain.user.service.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -14,32 +15,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/chat/room")
+@RequestMapping(value = "/api/chat/{roomId}")
 public class ChatController {
 
     private final SimpMessagingTemplate template; // Broker
     private final GetMessageListService getMessageListService;
     private final SaveMessageService saveMessageService;
+    private final UserFacade userFacade;
 
     //Client 가 SEND 할 수 있는 경로
     //stompConfig 에서 설정한 applicationDestinationPrefixes 와 @MessageMapping 경로가 병합됨
     //"/pub/chat/enter"
+
+    // 메시지 관련 API
+
     @MessageMapping(value = "/chat/enter")
-    public void enter(MessageRequest message) {
+    public void enter(@PathVariable String roomId, MessageRequest message) {
+        message.setRoomId(roomId);
+        message.setWriter(userFacade.getInfo().getSid());
         message.setContent(message.getWriter() + "님이 채팅방에 참여하였습니다.");
         template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 
     @MessageMapping(value = "/chat/message")
-    public void message(MessageRequest message) {
+    public void message(@PathVariable String roomId, MessageRequest message) {
+        message.setRoomId(roomId);
+        message.setWriter(userFacade.getInfo().getSid());
         saveMessageService.execute(message);
 
         template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 
-    // 메시지 관련 API
 
-    @GetMapping(value = "/{roomId}")
+    @GetMapping(value = "/")
     public Iterable<ChatRecord> getRecord(@PathVariable String roomId) {
         return getMessageListService.execute(roomId);
     }
