e6e511604c7a8d5c8fb2edaed042f9ef4dbc7210
zios <khyunwook0707@gmail.com>
2023-11-28 17:28:01 +0900

    perf // getByViewId

--- src/main/java/com/findbydema/domain/boardFunc/comment/service/CommentFacade.java
+++ src/main/java/com/findbydema/domain/boardFunc/comment/service/CommentFacade.java
@@ -1,5 +1,6 @@
 package com.findbydema.domain.boardFunc.comment.service;
 
+import com.findbydema.domain.board.exception.NotExistViewIdException;
 import com.findbydema.domain.boardFunc.comment.entity.Comment;
 import com.findbydema.domain.boardFunc.comment.repository.CommentRepository;
 import com.findbydema.domain.user.entity.User;
@@ -15,9 +16,13 @@ public class CommentFacade {
     private final UserFacade userFacade;
 
     public boolean isOwn(String viewId) {
-        Comment comment = commentRepository.findByViewId(viewId)
-                .orElseThrow(() -> new RuntimeException("없는 뷰아이디"));
+        Comment comment = getByViewId(viewId);
         User ownUser = userFacade.getInfo();
         return comment.getWriterSid().equals(ownUser.getSid());
+    }
+
+    public Comment getByViewId(String viewId) {
+        return commentRepository.findByViewId(viewId)
+                .orElseThrow(() -> NotExistViewIdException.EXCEPTION);
     }
 }
