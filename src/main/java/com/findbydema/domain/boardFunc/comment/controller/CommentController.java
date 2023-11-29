474164713d8cb915d62b062d39e5214873b3cc66
zios <khyunwook0707@gmail.com>
2023-11-29 20:43:30 +0900

    feat // comment (Delete)

--- src/main/java/com/findbydema/domain/boardFunc/comment/controller/CommentController.java
+++ src/main/java/com/findbydema/domain/boardFunc/comment/controller/CommentController.java
@@ -5,6 +5,7 @@ import com.findbydema.domain.boardFunc.comment.controller.dto.request.PatchComme
 import com.findbydema.domain.boardFunc.comment.controller.dto.response.CommentResponse;
 import com.findbydema.domain.boardFunc.comment.entity.Comment;
 import com.findbydema.domain.boardFunc.comment.service.CreateCommentService;
+import com.findbydema.domain.boardFunc.comment.service.DeleteCommentService;
 import com.findbydema.domain.boardFunc.comment.service.GetListCommentService;
 import com.findbydema.domain.boardFunc.comment.service.PatchCommentService;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,7 @@ public class CommentController {
     private final CreateCommentService createCommentService;
     private final GetListCommentService getListCommentService;
     private final PatchCommentService patchCommentService;
+    private final DeleteCommentService deleteCommentService;
 
     @PostMapping("/post")
     public void PostComment(@PathVariable String viewId, @RequestBody CreateCommentRequest createCommentRequest) {
@@ -33,5 +35,10 @@ public class CommentController {
     @PatchMapping("/{commentId}")
     public void PatchComment(@PathVariable String commentId, @RequestBody PatchCommentRequest patchCommentRequest) {
         patchCommentService.execute(commentId, patchCommentRequest);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public void DeleteComment(@PathVariable String commentId) {
+        deleteCommentService.execute(commentId);
     }
 }
