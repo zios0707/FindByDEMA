749fcc6b6e63c08ba8735cc4fa68ffbe2dfe9b47
zios <khyunwook0707@gmail.com>
2023-11-29 20:18:56 +0900

    feat // comment (Delete)

---
+++ src/main/java/com/findbydema/domain/boardFunc/comment/service/DeleteCommentService.java
@@ -0,0 +1,24 @@
+package com.findbydema.domain.boardFunc.comment.service;
+
+import com.findbydema.domain.board.exception.NotOwnerException;
+import com.findbydema.domain.boardFunc.comment.entity.Comment;
+import com.findbydema.domain.boardFunc.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteCommentService {
+
+    private final CommentFacade commentFacade;
+    private final CommentRepository commentRepository;
+
+    public void execute(String viewId) {
+        Comment comment = commentFacade.getByViewId(viewId);
+        if(commentFacade.isOwn(viewId)) {
+            commentRepository.delete(comment);
+        }else {
+            throw NotOwnerException.EXCEPTION;
+        }
+    }
+}
